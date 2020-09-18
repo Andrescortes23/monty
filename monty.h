66335c0890ff29b00c62827cfe454e7f6d33bda9
Andrescortes23 <andresccortes23@icloud.com>
2020-09-18 04:01:28 +0000

    header

--- monty.h
+++ monty.h
@@ -55,6 +55,7 @@ void match(char *token, stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 
 
 #endif
