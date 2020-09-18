c0168e5583d2971594a058c312ae154afeef6939
Andrescortes23 <andresccortes23@icloud.com>
2020-09-18 04:25:38 +0000

    Monty project

--- functions.c
+++ functions.c
@@ -16,6 +16,7 @@ void match(char *token, stack_t **stack, unsigned int line_number)
 		{"pop", pop},
 		{"swap", swap},
 		{"add", add},
+		{"nop", nop},
 		{"NULL", NULL}
 	};
 
--- opcodes2.c
+++ opcodes2.c
@@ -23,3 +23,14 @@ void add(stack_t **stack, unsigned int line_number)
 	free(tmp);
 	(*stack)->prev = NULL;
 }
+
+/**
+ *nop - doesn't do anything
+ *@stack: stack
+ *@line_number: file lines number
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
