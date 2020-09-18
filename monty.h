965a213ec812b58d301df2c55bc2f7d58f94364d
Andrescortes23 <andresccortes23@icloud.com>
2020-09-18 02:57:56 +0000

    head

--- monty.h
+++ monty.h
@@ -53,6 +53,7 @@ void error(unsigned int line_number);
 void wrongnum(char *num, unsigned int lin_num, stack_t **stack, stack_t *new);
 void match(char *token, stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 
 #endif
